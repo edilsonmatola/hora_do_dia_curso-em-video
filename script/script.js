80647429547131f1e434f02c5ca4ef0d6d7c0c2f
Edilson Matola <edilsonmatola13@gmail.com>
2021-12-28 18:09:09 +0200

    Veja que horas são

---
+++ script/script.js
@@ -0,0 +1,23 @@
+function load() {
+  var msg = document.getElementById("msg");
+  var img = document.getElementById("imagem");
+
+  var data = new Date();
+  var hora = data.getHours();
+
+  msg.innerHTML = `Agora são ${hora} horas.`;
+
+  if (hora >= 0 && hora < 12) {
+    //   BOM DIA
+    img.src = "./images/morning.png";
+    document.body.style.background = "#e6e3db";
+  } else if (hora >= 12 && hora <= 18) {
+    //   BOA TARDE
+    img.src = "./images/tarde.png";
+    document.body.style.background = "#b9846f";
+  } else {
+    //   BOA NOITE
+    img.src = "./images/night.png";
+    document.body.style.background = "#323232";
+  }
+}
